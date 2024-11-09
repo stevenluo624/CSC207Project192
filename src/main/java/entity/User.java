7ad9a76fef661aad894890d36292b4ad4d96653b
codymavok <85233381+codymavok@users.noreply.github.com>
2024-11-09 15:43:58 -0500

    Added User entity

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -1,4 +1,17 @@
 package entity;
+/**
+ * The representation of a user in our program.
+ */
+public interface User {
+    /**
+     * Returns the username of the user.
+     * @return username of user.
+     */
+    String getUsername();
 
-public class User {
+    /**
+     * Returns the password of the user.
+     * @return password of the user.
+     */
+    String getPassword();
 }
