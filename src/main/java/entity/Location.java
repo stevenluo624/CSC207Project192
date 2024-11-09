61f6d51fc2af89244fd1a3a4c3f007db849268ef
hamaguchi <93605959+LeandroHamaguchi@users.noreply.github.com>
2024-11-09 16:20:13 -0500

    Create Location abstract class with a rating attribute

--- src/main/java/entity/Location.java
+++ src/main/java/entity/Location.java
@@ -3,4 +3,48 @@ package entity;
 import java.util.List;
 
 public abstract class Location {
+    String name;
+    String description = "";
+    String address = "";
+    float rating = 0;
+
+    protected Location(String name) {
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * Calculates the location's score based on a list of scores and updates rating
+     * @param scores list of floats representing scores for this location
+     */
+    public void setRating(List<Float> scores) {
+        float scoreAcc = this.rating;
+        for (float score : scores) {
+            scoreAcc += score;
+        }
+        this.rating = scoreAcc / scores.size();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public float getRating() {
+        return rating;
+    }
 }
