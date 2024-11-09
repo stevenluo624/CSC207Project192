7a5b1ad79aa0ef41932a5d75591a989b37614014
codymavok <85233381+codymavok@users.noreply.github.com>
2024-11-09 16:28:17 -0500

    Userreview-entity added

--- src/main/java/entity/UserReview.java
+++ src/main/java/entity/UserReview.java
@@ -1,6 +1,46 @@
 package entity;
+
 /**
  * The representation of a UserReview in our program.
  */
 public class UserReview {
+    private final User user;
+    private final double rating;
+    private final String comment;
+
+    /**
+     * Creates a new UserReview
+     * @param user The user who created the review
+     * @param rating The rating given in the review
+     * @param comment The comment provided in the review
+     */
+    public UserReview(User user, double rating, String comment) {
+        this.user = user;
+        this.rating = rating;
+        this.comment = comment;
+    }
+
+    /**
+     * Gets the username of the user who created this review
+     * @return the username
+     */
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    /**
+     * Gets the rating
+     * @return the rating
+     */
+    public double getRating() {
+        return rating;
+    }
+
+    /**
+     * Gets the comment
+     * @return the comment
+     */
+    public String getComment() {
+        return comment;
+    }
 }
