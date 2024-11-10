4472efee2d1e653fc6842f683df17fafb1b337f2
hamaguchi <93605959+LeandroHamaguchi@users.noreply.github.com>
2024-11-10 12:26:10 -0500

    Implement FoodLocation class with attribute type

--- src/main/java/entity/FoodLocation.java
+++ src/main/java/entity/FoodLocation.java
@@ -1,4 +1,14 @@
 package entity;
 
-public class FoodLocation {
+public class FoodLocation extends Location {
+    private String type; // type of food served at this location
+
+    public FoodLocation(String name, String type) {
+        super(name);
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
 }
