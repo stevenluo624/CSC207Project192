e102d9124af305ca5e15e944340e7ab0de602a63
Tianyu Luo <tianyuluo@Tianyus-MacBook-Air.local>
2024-11-09 20:30:38 -0500

    adding code for interface_adapters.login package

--- src/main/java/interface_adapters/login/LoginPresenter.java
+++ src/main/java/interface_adapters/login/LoginPresenter.java
@@ -1,4 +1,43 @@
 package interface_adapters.login;
 
-public class LoginPresenter {
+import interface_adapters.ViewManagerModel;
+import use_case.login.LoginOutputBoundary;
+import use_case.login.LoginOutputData;
+
+/**
+ * The Presenter for the Login Use Case.
+ */
+public class LoginPresenter implements LoginOutputBoundary {
+
+    private final LoginViewModel loginViewModel;
+    private final LoggedInViewModel loggedInViewModel;
+    private final ViewManagerModel viewManagerModel;
+
+    public LoginPresenter(ViewManagerModel viewManagerModel,
+                          LoggedInViewModel loggedInViewModel,
+                          LoginViewModel loginViewModel) {
+        this.viewManagerModel = viewManagerModel;
+        this.loggedInViewModel = loggedInViewModel;
+        this.loginViewModel = loginViewModel;
+    }
+
+    @Override
+    public void prepareSuccessView(LoginOutputData response) {
+        // On success, switch to the logged in view.
+
+        final LoggedInState loggedInState = loggedInViewModel.getState();
+        loggedInState.setUsername(response.getUsername());
+        this.loggedInViewModel.setState(loggedInState);
+        this.loggedInViewModel.firePropertyChanged();
+
+        this.viewManagerModel.setState(loggedInViewModel.getViewName());
+        this.viewManagerModel.firePropertyChanged();
+    }
+
+    @Override
+    public void prepareFailView(String error) {
+        final LoginState loginState = loginViewModel.getState();
+        loginState.setLoginError(error);
+        loginViewModel.firePropertyChanged();
+    }
 }
