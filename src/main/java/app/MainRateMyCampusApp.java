11f75cf471f26b4ff0e687629ac24d7aa4ae8f64
hamaguchi <93605959+LeandroHamaguchi@users.noreply.github.com>
2024-11-11 17:23:48 -0500

    Implemented Signup use case and fixed bugs in the builder

--- src/main/java/app/MainRateMyCampusApp.java
+++ src/main/java/app/MainRateMyCampusApp.java
@@ -1,16 +1,21 @@
 package app;
 
 import data_access.DBReviewAccessObject;
+import data_access.DBUserAccessObject;
+import use_case.create_review.CreateReviewDataAccessInterface;
+import use_case.login.LoginUserDataAccessInterface;
 
 public class MainRateMyCampusApp {
 
     public static void main(String[] args) {
         
         // Create data access objects to inject in the builder
-        final ReviewDataAccessInterface reviewDataAccess = new DBReviewAccessObject();
-        final UserDataAccessInterface userDataAccess = new DBUserAccessObject();
+        final CreateReviewDataAccessInterface reviewDataAccess = new DBReviewAccessObject();
+        final LoginUserDataAccessInterface userDataAccess = new DBUserAccessObject();
 
         final RateMyCampusAppBuilder builder = new RateMyCampusAppBuilder();
-        
+        builder.addRateView()
+                .addCreateReviewUseCase()
+                .build().setVisible(true);
     }
 }
