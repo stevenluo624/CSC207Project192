7da23e104305802c9c4c7e1678c5017749c80e66
Anthony Sun <tndlight@Anthonys-MacBook-Air-5.local>
2024-11-28 17:39:42 -0500

    delete unnessary use case

--- src/main/java/use_case/delete_review/DeleteReviewInteractor.java
+++
@@ -1,9 +0,0 @@
-package use_case.delete_review;
-
-/**
- * The Delete Review Interactor
- */
-public class DeleteReviewInteractor implements  DeleteReviewInputBoundary{
-    final DeleteReviewDataAccessInterface deleteReviewDataAccessInterface;
-    
-}
