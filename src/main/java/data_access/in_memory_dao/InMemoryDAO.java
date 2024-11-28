b7fd6f9b2e3c9b53c4df17545b1d82a02d6c9c5e
hamaguchi <93605959+LeandroHamaguchi@users.noreply.github.com>
2024-11-28 18:20:14 -0500

    Changes to use case test

---
+++ src/main/java/data_access/in_memory_dao/InMemoryDAO.java
@@ -0,0 +1,5 @@
+package data_access.in_memory_dao;
+
+public abstract class InMemoryDAO {
+
+}
--- src/test/java/use_case/logout/CreateReviewUseCaseTest.java
+++ src/test/java/use_case/logout/CreateReviewUseCaseTest.java
@@ -1,26 +1,63 @@
 package use_case.logout;
 
+import data_access.in_memory_dao.InMemoryDAO;
+import entity.reviews_thread.Review;
 import org.junit.Before;
+import org.junit.jupiter.api.BeforeEach;
 import use_case.create_review.*;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class CreateReviewUseCaseTest {
-    private CreateReviewInteractor createReviewInteractor;
+    private CreateReviewInteractor interactor;
     private CreateReviewOutputBoundary presenter;
     private InMemoryDAO inMemoryDAO;
 
     @BeforeEach
     public void setUp() {
-        presenter = mock(CreateReviewOutputBoundary.class);
-    }
-
-    private CreateReviewOutputBoundary mock(Class<CreateReviewOutputBoundary> createReviewOutputBoundaryClass) {
+        presenter = CreateReviewOutputBoundary;
+        inMemoryDAO = InMemoryDAO;
+        interactor = new CreateReviewInteractor(inMemoryDAO, presenter);
     }
 
     @Test
     public void successfulCreateReview() {
+        CreateReviewDataAccessInterface noteDAO = new CreateReviewDataAccessInterface() {
 
+
+            /**
+             * @param review contains details of the review
+             * @return
+             */
+            @Override
+            public String saveReview(Review review) {
+                return "";
+            }
+
+            /**
+             * @param id the review id
+             * @return
+             */
+            @Override
+            public Review getReview(String id) {
+                return null;
+            }
+
+        CreateReviewOutputBoundary noteOB = new CreateReviewIOutputBoundary {
+            @Override
+            public void prepareSuccessView(String message) {
+                assertEquals("test", message);
+            }
+
+            @Override
+            public void prepareFailView(String errorMessage) {
+                fail(errorMessage);
+            }
+        };
+
+        NoteInteractor noteInteractor = new NoteInteractor(noteDAO, noteOB);
+
+        noteInteractor.executeRefresh();
     }
 }
